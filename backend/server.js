2237da1edcc257a7b2d70e877be9d3c44368cbb0
Poornima <poornima@gmail.com>
2026-05-11 20:33:52 +0530

    contact form added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const Contact = require("./models/Contact");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -6,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb+srv://poornima:<poorni123>@cluster0.dlajrb0.mongodb.net/")
+mongoose.connect("mongodb://poornima:poorni123@ac-8i3l7xb-shard-00-00.dlajrb0.mongodb.net:27017,ac-8i3l7xb-shard-00-01.dlajrb0.mongodb.net:27017,ac-8i3l7xb-shard-00-02.dlajrb0.mongodb.net:27017/?ssl=true&replicaSet=atlas-bg69kw-shard-0&authSource=admin&appName=Cluster0")
 
 .then(() => {
 
@@ -23,6 +24,27 @@ mongoose.connect("mongodb+srv://poornima:<poorni123>@cluster0.dlajrb0.mongodb.ne
 app.get("/", (req, res) => {
 
     res.send("Backend Working");
+
+});
+app.post("/contact", async (req, res) => {
+
+    try {
+
+        const newContact = new Contact(req.body);
+
+        await newContact.save();
+
+        res.send("Message Saved");
+
+    } 
+    
+    catch (error) {
+
+        console.log(error);
+
+        res.send("Error");
+
+    }
 
 });
 
