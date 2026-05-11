4a1b2216179771278105d394ae3bfbffdfc93282
Poornima <poornima@gmail.com>
2026-05-11 20:39:57 +0530

    final update

--- script.js
+++ script.js
@@ -40,3 +40,38 @@ async function getMessage(){
 }
 
 getMessage();
+async function sendMessage() {
+
+    const name = document.getElementById("name").value;
+
+    const email = document.getElementById("email").value;
+
+    const message = document.getElementById("message").value;
+
+    const response = await fetch("http://localhost:5000/contact", {
+
+        method: "POST",
+
+        headers: {
+
+            "Content-Type": "application/json"
+
+        },
+
+        body: JSON.stringify({
+
+            name,
+
+            email,
+
+            message
+
+        })
+
+    });
+
+    const data = await response.text();
+
+    alert(data);
+
+}
